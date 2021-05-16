c4644bd7a340a00d455cc5074573cd5d9cbf0c5d
Bobby Keel <bobbykeel76@gmail.com>
2021-05-16 04:31:58 -0500

    javascript file setup with instructions

---
+++ JavaScript/dateActProfilesGenerator.js
@@ -0,0 +1,22 @@
+// Date Act is an app that will allow the user to select attribute classes (gender, age range, and type), then use those attributes to randomly generate a famous character profile, or generate a random not famous profile from multiple attribute classes. should the user select create your own the user will be prompted by a profile form to complete which will store the character profile for use by anyone. should the user search for a character with keywords the profiles will list on the page for the user to click and open profile. then user will be able to save the profile to their account for later use.
+
+
+
+// create a object called profile that randomly generates each charactor attribute or character based on the selections from the form
+
+
+// build each random generator function that will generate each profile or profile attribute in the previous profile object
+
+
+// build a function that will attached the profile to the DOM
+
+
+// create each list of attributes to randomly choose from
+
+
+// create a list of famous profiles or famous attributes to add to a famous character
+
+// build a function to create and store a create your own profile, allow this profile to be used by the attaching to the dom function
+
+// build a function that will allow users to search the current lists of characters by keywords
+
